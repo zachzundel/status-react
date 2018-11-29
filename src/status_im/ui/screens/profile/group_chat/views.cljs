3b70066e80d89e32ce601cc3b7dde8a542597f89
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2018-11-29 11:06:06 +0100

    Allow admin to leave chat

--- src/status_im/ui/screens/profile/group_chat/views.cljs
+++ src/status_im/ui/screens/profile/group_chat/views.cljs
@@ -52,17 +52,24 @@
      :action              #(re-frame/dispatch [:group-chats.ui/remove-chat-pressed chat-id])
      :accessibility-label :delete-chat-button}]))
 
-(defn member-actions [chat-id member]
+(defn member-actions [chat-id member us-admin?]
   [{:action #(re-frame/dispatch [(if platform/desktop? :show-profile-desktop :chat.ui/show-profile) (:public-key member)])
     :label  (i18n/label :t/view-profile)}
-   {:action #(re-frame/dispatch [:group-chats.ui/remove-member-pressed chat-id (:public-key member)])
-    :label  (i18n/label :t/remove-from-chat)}])
+   (when-not (:admin? member)
+     {:action #(re-frame/dispatch [:group-chats.ui/remove-member-pressed chat-id (:public-key member)])
+      :label  (i18n/label :t/remove-from-chat)})
+   (when (and us-admin?
+              (not (:admin? member)))
+     {:action #(re-frame/dispatch [:group-chats.ui/make-admin-pressed chat-id (:public-key member)])
+      :label  (i18n/label :t/make-admin)})])
 
 (defn render-member [chat-id {:keys [name public-key] :as member} admin? current-user-identity]
   [react/view
    [contact/contact-view
     {:contact             member
-     :extend-options      (member-actions chat-id member)
+     :extend-options      (member-actions chat-id member admin?)
+     :info                (when (:admin? member)
+                            (i18n/label :t/group-chat-admin))
      :extend-title        name
      :extended?           (and admin?
                                (not= public-key current-user-identity))
@@ -90,24 +97,25 @@
             editing?     [:get :group-chat-profile/editing?]
             changed-chat [:get :group-chat-profile/profile]
             current-pk   [:account/public-key]]
-    (let [shown-chat (merge current-chat changed-chat)
-          admin?     (admins current-pk)]
-      [react/view profile.components.styles/profile
-       [status-bar/status-bar]
-       (if editing?
-         [group-chat-profile-edit-toolbar]
-         [group-chat-profile-toolbar admin?])
-       [react/scroll-view
-        [react/view profile.components.styles/profile-form
-         [profile.components/profile-header
-          {:contact              shown-chat
-           :editing?             editing?
-           :allow-icon-change?   false
-           :on-change-text-event :group-chats.ui/name-changed}]
-         [list/action-list (actions admin? chat-id)
-          {:container-style        styles/action-container
-           :action-style           styles/action
-           :action-label-style     styles/action-label
-           :action-separator-style styles/action-separator
-           :icon-opts              styles/action-icon-opts}]
-         [members-list chat-id admin? (first admins) current-pk]]]])))
+    (when current-chat
+      (let [shown-chat (merge current-chat changed-chat)
+            admin?     (admins current-pk)]
+        [react/view profile.components.styles/profile
+         [status-bar/status-bar]
+         (if editing?
+           [group-chat-profile-edit-toolbar]
+           [group-chat-profile-toolbar admin?])
+         [react/scroll-view
+          [react/view profile.components.styles/profile-form
+           [profile.components/profile-header
+            {:contact              shown-chat
+             :editing?             editing?
+             :allow-icon-change?   false
+             :on-change-text-event :group-chats.ui/name-changed}]
+           [list/action-list (actions admin? chat-id)
+            {:container-style        styles/action-container
+             :action-style           styles/action
+             :action-label-style     styles/action-label
+             :action-separator-style styles/action-separator
+             :icon-opts              styles/action-icon-opts}]
+           [members-list chat-id admin? (first admins) current-pk]]]]))))
